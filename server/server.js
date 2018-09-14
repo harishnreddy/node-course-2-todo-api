768ffe9433f3794fd00a5fc4e1b3f44a2026ec80
harishnreddy <harishnreddy@gmail.com>
2018-09-14 09:12:05 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -36,6 +37,33 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 
+
+});
+
+// GET /todos/12345
+
+app.get('/todos/:id', (req,res) => {
+  //res.send(req.params);
+
+  var id = req.params.id;
+
+  //validate id - 404
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  //id = '6b99897cacff23df9cf781d9';
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      console.log("have No user");
+      res.status(404).send();
+    } else {
+      res.send({todo});
+    }
+  }).catch((e) => {
+    res.status(400).send();
+  });
 
 });
 
