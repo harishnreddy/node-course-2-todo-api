a81673225d5d35ce212f462e26bd887a2e1be22d
harishnreddy <harishnreddy@gmail.com>
2018-09-19 15:20:51 -0400

    delete

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -118,6 +118,68 @@ describe('GET /todos/:id', () => {
 
 });
 
+
+describe('DELETE /todos/:id', () => {
+
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    console.log(hexId);
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+//
+
+  it('should return a 404 if todo is not found', (done) => {
+    var hexId = '5ba28274c51028c3627c80ea';
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 if todo is not found', (done) => {
+    var hexId = '5ba28274c51028c3627c80ea11';
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+//
+
+
+});
+
+
+  // it('should return a 404 if todo not found', (done) => {
+  //
+  //
+  // });
+  //
+  // it('should return a 404 if object id is invalid', (done) => {
+  //
+  //
+  // });
+
+
 //2
 
 //3
